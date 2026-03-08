9f7014b1b0b642bfa16bd8b34be9ecf0f38ca10e
Joker <joker@mac.home>
2026-03-08 17:32:56 +0100

    Update Transaction and adding Block

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,9 +1,10 @@
 mod wallet;
 mod transaction;
-
+mod block;
 
 use wallet::*;
 use transaction::*;
+use block::*;
 
 #[cfg(test)]
 mod test;
--- src/models/transaction.rs
+++ src/models/transaction.rs
@@ -22,8 +22,25 @@ pub struct Transaction {
     pub status: TransactionStatus,
 }
 
+impl Default for Transaction {
+    fn default() -> Self {
+        Self {
+            sender: String::new(),
+            recipient: String::new(),
+            amount: 0,
+            fee: 0,
+            signature: String::new(),
+            status: TransactionStatus::Pending,
+        }
+    }
+}
+
 impl Transaction {
     pub fn new(sender: &Wallet, recipient: &Wallet, amount: u64, fee: u64) -> Self {
         Self { sender: sender.address.clone(), recipient: recipient.address.clone(), amount, fee, signature: String::new(), status: TransactionStatus::Pending }
+    }
+    
+    pub fn is_empty(&self) -> bool {
+        self.sender.is_empty() && self.recipient.is_empty() && self.amount == 0
     }
 }
